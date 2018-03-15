1942855238b1e7847d10ba53fd9455f7d4600098
Mohammad Sadiq <mohammad_sadiq@dell.com>
2018-03-15 14:16:41 -0500

    changes for homecomponent

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -10,7 +10,7 @@ import { TagService } from '../entities/tag/tag.service';
 import { Article } from '../entities/article/article.model';
 import { Tag } from '../entities/tag/tag.model';
 import { JhiEventManager, JhiAlertService } from 'ng-jhipster';
-
+import * as Chartist from 'chartist';
 import { Account, LoginModalService, Principal } from '../shared';
 
 @Component({
@@ -31,9 +31,11 @@ export class HomeComponent implements OnInit {
     predicate: any;
     reverse: any;
     totalStations: number;
-    totalEngines: number;
+    totalItems: number;
     totalArticles: number;
     totalTags: number;
+    
+
     constructor(
         private principal: Principal,
         private loginModalService: LoginModalService,
@@ -43,8 +45,80 @@ export class HomeComponent implements OnInit {
         private articleService: ArticleService,
         private tagService: TagService,
         private jhiAlertService: JhiAlertService
-        ) {
+    ) {
     }
+
+    chartOptions = {
+        responsive: true
+    };
+
+    chartData = [
+        { data: [330, 600, 260, 700, 900], label: 'Outstanding' },
+        { data: [120, 455, 100, 340, 220], label: 'Good' },
+        { data: [45, 67, 800, 500, 200], label: 'Poor' }
+    ];
+
+    chartLabels = ['Station', 'Engine', 'Job', 'Hazards', 'Lessons'];
+
+    onChartClick(event) {
+        console.log(event);
+    }
+
+    drawPieChart() {
+        const dataEmailsSubscriptionChart = {
+            labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+            series: [
+                [542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895]
+
+            ]
+        };
+        const optionsEmailsSubscriptionChart = {
+            axisX: {
+                showGrid: false
+            },
+            low: 0,
+            high: 1000,
+            chartPadding: { top: 0, right: 5, bottom: 0, left: 0 }
+        };
+        const responsiveOptions: any[] = [
+            ['screen and (max-width: 640px)', {
+                seriesBarDistance: 5,
+                axisX: {
+                    labelInterpolationFnc: function (value) {
+                        return value[0];
+                    }
+                }
+            }]
+        ];
+        const emailsSubscriptionChart = new Chartist.Bar('#emailsSubscriptionChart', dataEmailsSubscriptionChart, optionsEmailsSubscriptionChart, responsiveOptions);
+
+        //start animation for the Emails Subscription Chart
+        this.startAnimationForBarChart(emailsSubscriptionChart);
+    }
+
+    startAnimationForBarChart(chart) {
+        let seq2: any, delays2: any, durations2: any;
+
+        seq2 = 0;
+        delays2 = 80;
+        durations2 = 500;
+        chart.on('draw', function (data) {
+            if (data.type === 'bar') {
+                seq2++;
+                data.element.animate({
+                    opacity: {
+                        begin: seq2 * delays2,
+                        dur: durations2,
+                        from: 0,
+                        to: 1,
+                        easing: 'ease'
+                    }
+                });
+            }
+        });
+
+        seq2 = 0;
+    };
 
     sort() {
         const result = [this.predicate + ',' + (this.reverse ? 'asc' : 'desc')];
@@ -55,45 +129,58 @@ export class HomeComponent implements OnInit {
     }
 
     loadAll() {
+        if (!this.isAuthenticated) {
+            return;
+        }
         this.stationService.query({
             page: 0,
             size: 100,
-            sort: ''}).subscribe(
-                (res: HttpResponse<Station[]>) => this.onSuccessStationQuery(res.body, res.headers),
-                (res: HttpErrorResponse) => this.onError(res.message)
-        );
+            sort: ''
+        }).subscribe(
+            (res: HttpResponse<Station[]>) => this.onSuccess(res.body, res.headers),
+            (res: HttpErrorResponse) => this.onError(res.message)
+            );
 
         this.engineService.query({
             page: 0,
             size: 100,
-            sort: ''}).subscribe(
-                (res: HttpResponse<Engine[]>) => this.onSuccessEngineQuery(res.body, res.headers),
-                (res: HttpErrorResponse) => this.onError(res.message)
-        );
+            sort: ""
+        }).subscribe(
+            (res: HttpResponse<Engine[]>) => this.onSuccessEngineQuery(res.body, res.headers),
+            (res: HttpErrorResponse) => this.onError(res.message)
+            );
 
         this.articleService.query({
             page: 0,
             size: 1000,
-            sort: ''}).subscribe(
-                (res: HttpResponse<Article[]>) => this.onSuccessArticlesQuery(res.body, res.headers),
-                (res: HttpErrorResponse) => this.onError(res.message)
-        );
+            sort: ''
+        }).subscribe(
+            (res: HttpResponse<Article[]>) => this.onSuccessArticlesQuery(res.body, res.headers),
+            (res: HttpErrorResponse) => this.onError(res.message)
+            );
 
         this.tagService.query({
             page: 0,
             size: 100,
-            sort: ''}).subscribe(
-                (res: HttpResponse<Tag[]>) => this.onSuccessTagsQuery(res.body, res.headers),
-                (res: HttpErrorResponse) => this.onError(res.message)
-        );
+            sort: ''
+        }).subscribe(
+            (res: HttpResponse<Tag[]>) => this.onSuccessTagsQuery(res.body, res.headers),
+            (res: HttpErrorResponse) => this.onError(res.message)
+            );
     }
 
     ngOnInit() {
+        this.totalArticles = 0;
+        this.totalItems = 0;
+        this.totalStations = 0;
+        this.totalTags = 0;
         this.loadAll();
+        this.drawPieChart();
         this.principal.identity().then((account) => {
             this.account = account;
         });
         this.registerAuthenticationSuccess();
+
     }
 
     getStationId(index: number, item: Station) {
@@ -105,7 +192,7 @@ export class HomeComponent implements OnInit {
     }
 
     getTotalEngines() {
-        return 'Total Engines : '.concat(this.totalEngines.toString());
+        return 'Total Engines : '.concat(this.totalItems.toString());
     }
 
     getTotalArticles() {
@@ -132,14 +219,17 @@ export class HomeComponent implements OnInit {
         this.modalRef = this.loginModalService.open();
     }
 
-    private onSuccessStationQuery(data, headers) {
+    private onSuccess(data, headers) {
+        // this.links = this.parseLinks.parse(headers.get('link'));
         this.totalStations = headers.get('X-Total-Count');
+        // this.queryCount = this.totalItems;
+        // this.page = pagingParams.page;
         this.stations = data;
     }
 
     private onSuccessEngineQuery(data, headers) {
         this.engines = data;
-        this.totalEngines = headers.get('X-Total-Count');
+        this.totalItems = headers.get('X-Total-Count');
     }
 
     private onSuccessArticlesQuery(data, headers) {
@@ -155,4 +245,5 @@ export class HomeComponent implements OnInit {
     private onError(error) {
         this.jhiAlertService.error(error.message, null, null);
     }
+
 }
